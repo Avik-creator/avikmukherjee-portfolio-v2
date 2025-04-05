26dab2b209883bb9f731af7f5c2752a2d2e799fd
Avik-creator <avikm744@gmail.com>
2025-04-05 17:06:16 +0530

    Made the Final Changes for Now.

--- components/Contact.tsx
+++ components/Contact.tsx
@@ -1,4 +1,5 @@
 import Link from "next/link"
+import CalButton from "./CallButton"
 
 interface SocialLink {
   name: string
@@ -15,19 +16,24 @@ export default function Contact({ socialLinks, email }: ContactProps) {
     <section>
       <h2 className="text-sm mb-4">reach</h2>
       <p className="leading-relaxed">
-        connect on{" "}
-        {socialLinks.map((link, index) => (
-          <span key={link.name}>
-            <Link href={link.url} className="underline">
-              {link.name}
-            </Link>
-            {index < socialLinks.length - 1 ? ", " : ""}
-          </span>
-        ))}{" "}
-        - or send me an email at{" "}
-        <Link href={`mailto:${email}`} className="underline">
-          {email}
+      connect on{" "}
+      {socialLinks.map((link, index) => (
+        <span key={link.name}>
+        <Link href={link.url} className="underline">
+          {link.name}
         </Link>
+        {index < socialLinks.length - 1 ? ", " : ""}
+        </span>
+      ))}{" "}
+      - or send me an email at{" "}
+      <Link href={`mailto:${email}`} className="underline">
+        {email}
+      </Link>
+      - or book a call with me using the button below.
+      <br />
+      <span className="flex justify-center mt-8 sm:mt-4 mb-4">
+        <CalButton />
+      </span>
       </p>
     </section>
   )
