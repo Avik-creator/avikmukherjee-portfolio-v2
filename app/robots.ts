5a5cea8276a8b8f871462dbf89fe3f2b9824dd99
Avik-creator <avikm744@gmail.com>
2025-06-27 11:10:31 +0530

    Added Changes.

--- app/robots.ts
+++ app/robots.ts
@@ -1,7 +1,7 @@
 import type {MetadataRoute} from "next";
 
 export default function robots(): MetadataRoute.Robots{
-    const baseURL = "https://www.avikmukherjee.tech";
+    const baseURL = "https://www.avikmukherjee.me";
     return{
         rules:{
             userAgent: "*",
