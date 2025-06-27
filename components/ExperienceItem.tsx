a32966563cf6782b0740241d3ec5256f8c47f8ff
Avik-creator <avikm744@gmail.com>
2025-06-27 12:25:45 +0530

    Added Changes.

--- components/ExperienceItem.tsx
+++ components/ExperienceItem.tsx
@@ -1,4 +1,5 @@
 import Anchor from "./Anchor"
+import { cn } from "@/lib/utils"
 
 interface ExperienceItemProps {
   title: string
@@ -12,7 +13,34 @@ export default function ExperienceItem({ title, year, company, description, comp
   return (
     <div className="mb-8">
       <div className="flex justify-between items-start mb-1">
-        <h3 className="font-medium text-neutral-100">{title}</h3>
+        <a
+          href={companySite}
+          target="_blank"
+          rel="noopener noreferrer"
+          className={cn(
+            'group flex items-center gap-1',
+            'hover:outline-[0.5px] outline-offset-[6px] hover:outline-neutral-600/50 border-0',
+            'relative'
+          )}
+        >
+          {/* Top left */}
+          <div
+            className="absolute left-[-6.25px] top-[-6.25px] hidden group-hover:block"
+            style={{ '--cross-size': '10px' } as React.CSSProperties}>
+            <div className="absolute left-0 top-0 h-[var(--cross-size)] w-[0.5px] -translate-x-1/2 -translate-y-1/2 bg-neutral-500" />
+            <div className="absolute left-0 top-0 h-[0.5px] w-[var(--cross-size)] -translate-x-1/2 -translate-y-1/2 bg-neutral-500" />
+          </div>
+          {/* Bottom right */}
+          <div
+            className="absolute bottom-[-6.25px] right-[-6.25px] hidden group-hover:block"
+            style={{ '--cross-size': '10px' } as React.CSSProperties}>
+            <div className="absolute left-0 top-0 h-[var(--cross-size)] w-[0.5px] -translate-x-1/2 -translate-y-1/2 bg-neutral-500" />
+            <div className="absolute left-0 top-0 h-[0.5px] w-[var(--cross-size)] -translate-x-1/2 -translate-y-1/2 bg-neutral-500" />
+          </div>
+          <h3 className="font-medium text-neutral-200 underline decoration-neutral-400/50 underline-offset-[3px] transition-colors group-hover:text-white">
+            {title} @{company}
+          </h3>
+        </a>
         <span className="text-neutral-500 text-sm">{year}</span>
       </div>
       
@@ -20,16 +48,6 @@ export default function ExperienceItem({ title, year, company, description, comp
         {description.map((desc, index) => (
           <p key={index} className="text-sm">• {desc}</p>
         ))}
-      </div>
-
-      <div>
-        <Anchor
-          href={companySite}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          {company}
-        </Anchor>
       </div>
     </div>
   )
