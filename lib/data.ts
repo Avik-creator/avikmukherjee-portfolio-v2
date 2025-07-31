f82440c46bfb8ea955af9120985e999fc337b3e3
Avik-creator <avikm744@gmail.com>
2025-08-01 00:00:08 +0530

    Added New Projects.

--- lib/data.ts
+++ lib/data.ts
@@ -1,10 +1,24 @@
 export const projects = [
   {
-    title:"WebTracker",
-    year:"2025",
-    description:"It is a powerful, privacy-focused web analytics platform that helps you to visualize the website's reach. Built it due to Vercel Analytics Quota exhaustion.",
-    demoUrl:"https://webtracker.avikmukherjee.me",
-    githubUrl:"https://github.com/Avik-creator/webtracker"
+    title: "NeuroSeek",
+    year: "2025",
+    description: "NeuroSeek is a AI Search engine that helps the users to search for information on the web in a comprehensive manner. It uses AI SDK to interact with all the models. Built it because I wanted to learn about AI Tool calling and a personal search engine.",
+    demoUrl: "https://neuroseek.avikmukherjee.me/",
+    githubUrl: "https://github.com/Avik-creator/neuroseek"
+  },
+  {
+    title: "ChessAI",
+    year: "2025",
+    description: "It is a Chess game built with NextJS and AI SDK. In here the user can play against AI and can improve their skills. Built it because I suck at Chess.",
+    demoUrl: "https://chess.avikmukherjee.me/",
+    githubUrl: "https://github.com/Avik-creator/chess"
+  },
+  {
+    title: "WebTracker",
+    year: "2025",
+    description: "It is a powerful, privacy-focused web analytics platform that helps you to visualize the website's reach. Built it due to Vercel Analytics Quota exhaustion.",
+    demoUrl: "https://webtracker.avikmukherjee.me",
+    githubUrl: "https://github.com/Avik-creator/webtracker"
   },
   {
     title: "AI PPT Generator",
@@ -13,176 +27,175 @@ export const projects = [
     demoUrl: "https://aipptgenerator.vercel.app/",
     githubUrl: "https://github.com/Avik-creator/aipptgenerator"
   },
-    {
-      title: "FormCraftAI",
-      year: "2025",
-      description:
+  {
+    title: "FormCraftAI",
+    year: "2025",
+    description:
       "Build professional forms in seconds with AI. Whether it is surveys, registrations, or feedback, describe your need and get a smart, user-friendly form instantly. Hosted on Vercel.",
-      demoUrl: "https://formcraftai-delta.vercel.app/",
-      githubUrl: "https://github.com/Avik-creator/aiformbuilder",
-    },
-    {
-      title: "RealtimeSudoku",
-      year: "2025",
-      description:
+    demoUrl: "https://formcraftai-delta.vercel.app/",
+    githubUrl: "https://github.com/Avik-creator/aiformbuilder",
+  },
+  {
+    title: "RealtimeSudoku",
+    year: "2025",
+    description:
       "Play Sudoku with your friends in real-time. Different difficulty levels available.",
-      demoUrl: "https://sudoku.avikmukherjee.me/",
-      githubUrl: "https://github.com/Avik-creator/soduku-realtime",
-    },
-    {
-      title: "CodeIDE",
-      year: "2024",
-      description:
+    demoUrl: "https://sudoku.avikmukherjee.me/",
+    githubUrl: "https://github.com/Avik-creator/soduku-realtime",
+  },
+  {
+    title: "CodeIDE",
+    year: "2024",
+    description:
       "It is a web-based code editor that supports multiple languages. It is built from Scratch using Nextjs, TailwindCSS, and Monaco Editor for the Frontend and Nodejs along with Socket.io for the backend. Dockerized the application for easy deployment.",
-      demoUrl: "https://x.com/avikm744/status/1842055111314383191",
-      githubUrl: "https://github.com/Avik-creator/codeide-codespace",
-    },
-    {
-      title: "ResumeWorth",
-      year: "2024",
-      description:
+    demoUrl: "https://x.com/avikm744/status/1842055111314383191",
+    githubUrl: "https://github.com/Avik-creator/codeide-codespace",
+  },
+  {
+    title: "ResumeWorth",
+    year: "2024",
+    description:
       "Enhance, evaluate, and roast resumes with AI-driven insights. Used by 700+ users. Built using Groq and LLaMA.",
-      demoUrl: "https://resume-worth-murex.vercel.app/",
-      githubUrl: "https://github.com/Avik-creator/resume-worth",
-    },
-    {
-      title: "AnimeStreamer",
-      year: "2024",
-      description:
+    demoUrl: "https://resume-worth-murex.vercel.app/",
+    githubUrl: "https://github.com/Avik-creator/resume-worth",
+  },
+  {
+    title: "AnimeStreamer",
+    year: "2024",
+    description:
       "A React + Zustand based web app to stream anime. Simple and fast UI with TypeScript. Currently not working due to Consumet API error.",
-      demoUrl: "https://animestreamer.vercel.app",
-      githubUrl: "https://github.com/Avik-creator/animestreamer",
-    },
-    {
-      title: "SQLite Viewer",
-      year: "2024",
-      description:
+    demoUrl: "https://animestreamer.vercel.app",
+    githubUrl: "https://github.com/Avik-creator/animestreamer",
+  },
+  {
+    title: "SQLite Viewer",
+    year: "2024",
+    description:
       "A web-based viewer to open SQLite DB files using sql.js. Built using NextJS and ShadCN.",
-      demoUrl: "https://sqliteviewer.vercel.app",
-      githubUrl: "https://github.com/Avik-creator/sqliteviewer",
-    },
-    {
-      title: "Payment Template Package",
-      year: "2023",
-      description:
+    demoUrl: "https://sqliteviewer.vercel.app",
+    githubUrl: "https://github.com/Avik-creator/sqliteviewer",
+  },
+  {
+    title: "Payment Template Package",
+    year: "2023",
+    description:
       "A NPM package that helps integrate payment gateways like Razorpay, Stripe into backend apps. Used by 500+ developers globally.",
-      demoUrl: "https://www.npmjs.com/package/payment-templates",
-      githubUrl: "https://github.com/Avik-creator/payment-templates-package",
-    },
-    {
-      title: "Hotel Booking App",
-      year: "2023",
-      description:
+    demoUrl: "https://www.npmjs.com/package/payment-templates",
+    githubUrl: "https://github.com/Avik-creator/payment-templates-package",
+  },
+  {
+    title: "Hotel Booking App",
+    year: "2023",
+    description:
       "A fullstack hotel booking platform built with React, Node.js, MongoDB. Supports Stripe payments and e2e testing with Playwright.",
-      demoUrl: "https://github.com/Avik-creator/Hotel-Booking-App",
-      githubUrl: "https://github.com/Avik-creator/Hotel-Booking-App",
-    },
-    {
-      title: "Sorting Visualizer",
-      year: "2023",
-      description:
+    demoUrl: "https://github.com/Avik-creator/Hotel-Booking-App",
+    githubUrl: "https://github.com/Avik-creator/Hotel-Booking-App",
+  },
+  {
+    title: "Sorting Visualizer",
+    year: "2023",
+    description:
       "Visualize sorting algorithms like Bubble Sort, Merge Sort, and Quick Sort. Built with React and TailwindCSS.",
-      demoUrl: "https://sorting-visualizer-three-olive.vercel.app/",
-      githubUrl: "https://github.com/Avik-creator/sorting-visualizer",
-    },
-    {
-      title: "Code to Picture",
-      year: "2023",
-      description:
+    demoUrl: "https://sorting-visualizer-three-olive.vercel.app/",
+    githubUrl: "https://github.com/Avik-creator/sorting-visualizer",
+  },
+  {
+    title: "Code to Picture",
+    year: "2023",
+    description:
       "Convert code snippets to beautiful images for sharing. Built using React and react-simple-code-editor.",
-      demoUrl: "https://code-to-picture.vercel.app/",
-      githubUrl: "https://github.com/Avik-creator/Code-to-Picture",
-    }
-    ]
+    demoUrl: "https://code-to-picture.vercel.app/",
+    githubUrl: "https://github.com/Avik-creator/Code-to-Picture",
+  }
+]
 
-    export const Experience = [
-      {
-        title: "Software Engineer Intern",
-        year: "May 2025 - Present",
-        company: "SuperAlign",
-        description: [
-          "Worked on the Frontend using NextJS, ShadcnUI, TailwindCSS and TypeScript",
-          "Worked on Backend using HonoJS, DuckDB",
-          "Implemented a python webscrapper to scrape data from the web and store it in a markdown file",
-        ],
-        companySite: "https://www.superalign.ai"
-      },
+export const Experience = [
+  {
+    title: "Software Engineer Intern",
+    year: "May 2025 - Present",
+    company: "SuperAlign",
+    description: [
+      "Worked on the Frontend using NextJS, ShadcnUI, TailwindCSS and TypeScript",
+      "Worked on Backend using HonoJS, DuckDB",
+      "Implemented a python webscrapper to scrape data from the web and store it in a markdown file",
+    ],
+    companySite: "https://www.superalign.ai"
+  },
 
-      {
-        title: "Junior Engineer Trainee (Intern)",
-        year: "Sept 2024 – March 2025",
-        company: "DataFoundry",
-        description: [
-          "Worked on Azure Data pipelines for data preprocessing and generating CSV files.",
-          "Implemented Localizations for the web application using i18next and React.",
-          "Implemented Permission Based Access Control for the web application."
-        ],
-        companySite: "https://datafoundry.ai"
-      },
-      {
-        title: "Junior React Native Developer (Intern)",
-        year: "Nov 2023 – Feb 2024",
-        company: "Dank",
-        description: [
-          "Collaborated with the development team to ensure timely delivery of applications.",
-          "Led feature development for core 40% of the application, with a focus on performance optimization.",
-          "Built and maintained core features like feed, comments, and user profiles.",
-          "Worked on the frontend using React Native."
-        ],
-        companySite: "https://play.google.com/store/apps/details?id=com.joindank.dankapp"
-      },
-      {
-        title: "LEAD",
-        year: "July 2023 – July 2024",
-        company: "GDG",
-        description: [
-          "Organized workshops, hackathons, and events to help students grow.",
-          "Conducted sessions on technologies like Flutter and Google Cloud Platform.",
-          "Built a community of 600+ tech enthusiasts.",
-          "Collaborated with organizations for event execution."
-        ],
-        companySite: "https://gdg.community.dev/gdg-on-campus-maulana-abul-kalam-azad-university-of-technology-kalyani-india/"
-      },
-      {
-        title: "Web Developer",
-        year: "July 2022 – July 2023",
-        company: "Repl",
-        description: [
-          "Built the frontend using React and plain CSS.",
-          "Deployed the website on Firebase."
-        ],
-        companySite: "https://repl-us.web.app"
-      },
-      {
-        title: "Software Developer (Intern)",
-        year: "June 2023 – Aug 2023",
-        company: "Auctopustechnologies",
-        description: [
-          "Created responsive interfaces with React and efficient API integrations.",
-          "Implemented Redux for managing network discovery states and automated subnet reporting (30% system response improvement).",
-          "Wrote Cypress test cases, reducing manual testing by 50%."
-        ],
-        companySite: "https://www.auctopustechnologies.com/home"
-      },
-      {
-        title: "Content Writer (Intern)",
-        year: "Nov 2022 – Feb 2023",
-        company: "Framedmedia",
-        description: [
-          "Wrote technical articles on React, Node, and miscellaneous topics like gaming.",
-          "Authored 50+ articles during the internship."
-        ],
-        companySite: "https://framedmedia.com"
-      },
-      {
-        title: "Content Writer (Intern)",
-        year: "May 2019 – Aug 2019",
-        company: "Qwerty Thoughts",
-        description: [
-          "Wrote spiritual articles focusing on Hindu mythology and spirituality.",
-          "Authored 30+ articles during the internship."
-        ],
-        companySite: "https://www.linkedin.com/company/qwerty-thoughts"
-      }
-    ];
-    
+  {
+    title: "Junior Engineer Trainee (Intern)",
+    year: "Sept 2024 – March 2025",
+    company: "DataFoundry",
+    description: [
+      "Worked on Azure Data pipelines for data preprocessing and generating CSV files.",
+      "Implemented Localizations for the web application using i18next and React.",
+      "Implemented Permission Based Access Control for the web application."
+    ],
+    companySite: "https://datafoundry.ai"
+  },
+  {
+    title: "Junior React Native Developer (Intern)",
+    year: "Nov 2023 – Feb 2024",
+    company: "Dank",
+    description: [
+      "Collaborated with the development team to ensure timely delivery of applications.",
+      "Led feature development for core 40% of the application, with a focus on performance optimization.",
+      "Built and maintained core features like feed, comments, and user profiles.",
+      "Worked on the frontend using React Native."
+    ],
+    companySite: "https://play.google.com/store/apps/details?id=com.joindank.dankapp"
+  },
+  {
+    title: "LEAD",
+    year: "July 2023 – July 2024",
+    company: "GDG",
+    description: [
+      "Organized workshops, hackathons, and events to help students grow.",
+      "Conducted sessions on technologies like Flutter and Google Cloud Platform.",
+      "Built a community of 600+ tech enthusiasts.",
+      "Collaborated with organizations for event execution."
+    ],
+    companySite: "https://gdg.community.dev/gdg-on-campus-maulana-abul-kalam-azad-university-of-technology-kalyani-india/"
+  },
+  {
+    title: "Web Developer",
+    year: "July 2022 – July 2023",
+    company: "Repl",
+    description: [
+      "Built the frontend using React and plain CSS.",
+      "Deployed the website on Firebase."
+    ],
+    companySite: "https://repl-us.web.app"
+  },
+  {
+    title: "Software Developer (Intern)",
+    year: "June 2023 – Aug 2023",
+    company: "Auctopustechnologies",
+    description: [
+      "Created responsive interfaces with React and efficient API integrations.",
+      "Implemented Redux for managing network discovery states and automated subnet reporting (30% system response improvement).",
+      "Wrote Cypress test cases, reducing manual testing by 50%."
+    ],
+    companySite: "https://www.auctopustechnologies.com/home"
+  },
+  {
+    title: "Content Writer (Intern)",
+    year: "Nov 2022 – Feb 2023",
+    company: "Framedmedia",
+    description: [
+      "Wrote technical articles on React, Node, and miscellaneous topics like gaming.",
+      "Authored 50+ articles during the internship."
+    ],
+    companySite: "https://framedmedia.com"
+  },
+  {
+    title: "Content Writer (Intern)",
+    year: "May 2019 – Aug 2019",
+    company: "Qwerty Thoughts",
+    description: [
+      "Wrote spiritual articles focusing on Hindu mythology and spirituality.",
+      "Authored 30+ articles during the internship."
+    ],
+    companySite: "https://www.linkedin.com/company/qwerty-thoughts"
+  }
+];
