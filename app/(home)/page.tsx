d03feb86161f894f8c1e035f15eb949a4795991f
Avik-creator <avikm744@gmail.com>
2025-12-09 16:57:07 +0530

    Added Changes.

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -40,17 +40,17 @@ export default function Home() {
           <div
             className="absolute left-[-6.25px] top-[-6.25px] hidden group-hover:block"
             style={{ '--cross-size': '10px' } as React.CSSProperties}>
-            <div className="absolute left-0 top-0 h-[var(--cross-size)] w-[0.5px] -translate-x-1/2 -translate-y-1/2 bg-gray-500 dark:bg-neutral-500" />
-            <div className="absolute left-0 top-0 h-[0.5px] w-[var(--cross-size)] -translate-x-1/2 -translate-y-1/2 bg-gray-500 dark:bg-neutral-500" />
+            <div className="absolute left-0 top-0 h-(--cross-size).5px] -translate-x-1/2 -translate-y-1/2 bg-gray-500 dark:bg-neutral-500" />
+            <div className="absolute left-0 top-0 h-[0.5px] w-(--cross-size)nslate-x-1/2 -translate-y-1/2 bg-gray-500 dark:bg-neutral-500" />
           </div>
           {/* Bottom right */}
           <div
             className="absolute bottom-[-6.25px] right-[-6.25px] hidden group-hover:block"
             style={{ '--cross-size': '10px' } as React.CSSProperties}>
-            <div className="absolute left-0 top-0 h-[var(--cross-size)] w-[0.5px] -translate-x-1/2 -translate-y-1/2 bg-gray-500 dark:bg-neutral-500" />
-            <div className="absolute left-0 top-0 h-[0.5px] w-[var(--cross-size)] -translate-x-1/2 -translate-y-1/2 bg-gray-500 dark:bg-neutral-500" />
+            <div className="absolute left-0 top-0 h-(--cross-size).5px] -translate-x-1/2 -translate-y-1/2 bg-gray-500 dark:bg-neutral-500" />
+            <div className="absolute left-0 top-0 h-[0.5px] w-(--cross-size)nslate-x-1/2 -translate-y-1/2 bg-gray-500 dark:bg-neutral-500" />
           </div>
-          <h2 className="grow text-lg font-semibold text-gray-900 dark:text-neutral-100 underline decoration-gray-500 dark:decoration-neutral-400/50 underline-offset-[4px] transition-colors">
+          <h2 className="grow text-lg font-semibold text-gray-900 dark:text-neutral-100 underline decoration-gray-500 dark:decoration-neutral-400/50 underline-offset-4nsition-colors">
             work
           </h2>
         </Link>
@@ -82,17 +82,17 @@ export default function Home() {
           <div
             className="absolute left-[-6.25px] top-[-6.25px] hidden group-hover:block"
             style={{ '--cross-size': '10px' } as React.CSSProperties}>
-            <div className="absolute left-0 top-0 h-[var(--cross-size)] w-[0.5px] -translate-x-1/2 -translate-y-1/2 bg-gray-500 dark:bg-neutral-500" />
-            <div className="absolute left-0 top-0 h-[0.5px] w-[var(--cross-size)] -translate-x-1/2 -translate-y-1/2 bg-gray-500 dark:bg-neutral-500" />
+            <div className="absolute left-0 top-0 h-(--cross-size).5px] -translate-x-1/2 -translate-y-1/2 bg-gray-500 dark:bg-neutral-500" />
+            <div className="absolute left-0 top-0 h-[0.5px] w-(--cross-size)nslate-x-1/2 -translate-y-1/2 bg-gray-500 dark:bg-neutral-500" />
           </div>
           {/* Bottom right */}
           <div
             className="absolute bottom-[-6.25px] right-[-6.25px] hidden group-hover:block"
             style={{ '--cross-size': '10px' } as React.CSSProperties}>
-            <div className="absolute left-0 top-0 h-[var(--cross-size)] w-[0.5px] -translate-x-1/2 -translate-y-1/2 bg-gray-500 dark:bg-neutral-500" />
-            <div className="absolute left-0 top-0 h-[0.5px] w-[var(--cross-size)] -translate-x-1/2 -translate-y-1/2 bg-gray-500 dark:bg-neutral-500" />
+            <div className="absolute left-0 top-0 h-(--cross-size) w-[0.5px] -translate-x-1/2 -translate-y-1/2 bg-gray-500 dark:bg-neutral-500" />
+            <div className="absolute left-0 top-0 h-[0.5px] w-(--cross-size)nslate-x-1/2 -translate-y-1/2 bg-gray-500 dark:bg-neutral-500" />
           </div>
-          <h2 className="grow text-lg font-semibold text-gray-900 dark:text-neutral-100 underline decoration-gray-500 dark:decoration-neutral-400/50 underline-offset-[4px] transition-colors">
+          <h2 className="grow text-lg font-semibold text-gray-900 dark:text-neutral-100 underline decoration-gray-500 dark:decoration-neutral-400/50 underline-offset-4nsition-colors">
             experience
           </h2>
         </Link>
@@ -125,17 +125,17 @@ export default function Home() {
           <div
             className="absolute left-[-6.25px] top-[-6.25px] hidden group-hover:block"
             style={{ '--cross-size': '10px' } as React.CSSProperties}>
-            <div className="absolute left-0 top-0 h-[var(--cross-size)] w-[0.5px] -translate-x-1/2 -translate-y-1/2 bg-gray-500 dark:bg-neutral-500" />
-            <div className="absolute left-0 top-0 h-[0.5px] w-[var(--cross-size)] -translate-x-1/2 -translate-y-1/2 bg-gray-500 dark:bg-neutral-500" />
+            <div className="absolute left-0 top-0 h-(--cross-size).5px] -translate-x-1/2 -translate-y-1/2 bg-gray-500 dark:bg-neutral-500" />
+            <div className="absolute left-0 top-0 h-[0.5px] w-(--cross-size)nslate-x-1/2 -translate-y-1/2 bg-gray-500 dark:bg-neutral-500" />
           </div>
           {/* Bottom right */}
           <div
             className="absolute bottom-[-6.25px] right-[-6.25px] hidden group-hover:block"
             style={{ '--cross-size': '10px' } as React.CSSProperties}>
-            <div className="absolute left-0 top-0 h-[var(--cross-size)] w-[0.5px] -translate-x-1/2 -translate-y-1/2 bg-gray-500 dark:bg-neutral-500" />
-            <div className="absolute left-0 top-0 h-[0.5px] w-[var(--cross-size)] -translate-x-1/2 -translate-y-1/2 bg-gray-500 dark:bg-neutral-500" />
+            <div className="absolute left-0 top-0 h-(--cross-size).5px] -translate-x-1/2 -translate-y-1/2 bg-gray-500 dark:bg-neutral-500" />
+            <div className="absolute left-0 top-0 h-[0.5px] w-(--cross-size) -translate-x-1/2 -translate-y-1/2 bg-gray-500 dark:bg-neutral-500" />
           </div>
-          <h2 className="grow text-lg font-semibold text-gray-900 dark:text-neutral-100 underline decoration-gray-500 dark:decoration-neutral-400/50 underline-offset-[4px] transition-colors">
+          <h2 className="grow text-lg font-semibold text-gray-900 dark:text-neutral-100 underline decoration-gray-500 dark:decoration-neutral-400/50 underline-offset-4 transition-colors">
             writing
           </h2>
         </Link>
