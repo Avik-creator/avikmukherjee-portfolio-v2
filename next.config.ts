e256ba1269f3fd80147f32fcd4958241cb0a660a
Avik Mukherjee <77090462+Avik-creator@users.noreply.github.com>
2026-05-17 17:25:35 +0530

    Remove output export option from next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,6 @@ import type { NextConfig } from "next";
 import createMDX from "@next/mdx";
 
 const nextConfig: NextConfig = {
-  output: 'export',
   /* config options here */
   productionBrowserSourceMaps: true,
   webpack: (config) => {
